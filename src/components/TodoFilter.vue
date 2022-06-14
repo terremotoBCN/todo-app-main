9d9660701365f61aa32c3c1e5b478a98b477a7f0
Terremoto BCN <terremoto.news@gmail.com>
2022-06-15 01:32:19 +0200

    New TODO footer

---
+++ src/components/TodoFilter.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts"></script>
+
+<template>
+	<div class="todo-filter">
+		<ul>
+			<li>
+				<a class="todo-filter__opt todo-filter__opt--active" href="#">
+					All
+				</a>
+			</li>
+			<li>
+				<a class="todo-filter__opt" href="#"> Active </a>
+			</li>
+			<li>
+				<a class="todo-filter__opt" href="#"> Completed </a>
+			</li>
+		</ul>
+	</div>
+</template>
