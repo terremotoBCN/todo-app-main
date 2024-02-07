630c5d417f7d1b78cd7b66eb3bfd8d735401882f
agarciaBCN <agarcia.bcn21@gmail.com>
2024-02-07 11:49:28 +0100

    fix: eliminate theme behaviour

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,6 @@
 import { createApp, VueElement } from "vue";
 import App from "./App.vue";
+import "../node_modules/normalize.css/normalize.css";
 import "../src/scss/styles.scss";
 
-//	theme
-import { storedTheme, switchTheme } from "./ts/themes";
-
 createApp(App).mount("#app");
-// if (storedTheme)
-// document.documentElement.setAttribute("data-theme", storedTheme);
-
-const toggle = document.getElementById("theme-toggle");
-if (toggle) {
-	toggle.addEventListener("click", switchTheme);
-}
-
-//window.onload = () => switchTheme();
-// export const eventBus = createApp({});
